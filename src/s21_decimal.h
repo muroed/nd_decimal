4736b068f2abb1342516eba85120711e8c8d8f5a
nazeebod <nazeebod@student.21-school.ru>
2022-07-30 23:05:43 +0300

    :star: add(convert): add description of functions

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,9 +1,15 @@
 #ifndef SRC_S21_DECIMAL_H_
 #define SRC_S21_DECIMAL_H_
 
+#include "decimal_core.h"
 
 typedef struct {
     int bits[4];
 } s21_decimal;
+
+int s21_from_int_to_decimal(int src, s21_decimal *dst);
+int s21_from_float_to_decimal(float src, s21_decimal *dst);
+int s21_from_decimal_to_int(s21_decimal src, int *dst);
+int s21_from_decimal_to_float(s21_decimal src, float *dst);
 
 #endif  //  SRC_S21_DECIMAL_H_
