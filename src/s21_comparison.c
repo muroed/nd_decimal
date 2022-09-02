e076a4b4c696d85f08acf739fd5b77c2d73ce8fd
Nazeebo Delma <nazeebod@ox-q5.msk.21-school.ru>
2022-09-02 19:26:24 +0300

    :chart_with_upwards_trend: performace: ungrade comparison functions

--- src/s21_comparison.c
+++ src/s21_comparison.c
@@ -13,31 +13,7 @@ int s21_is_equal(s21_decimal decim1, s21_decimal decim2) {
 }
 
 int s21_is_not_equal(s21_decimal decim1, s21_decimal decim2) {
-  int res_equal = 0;
-  for (int i = 0; i < ALL_BIT; i++) {
-    if (get_global_bit(decim1, i) != get_global_bit(decim2, i)) {
-      res_equal = 1;
-      break;
-    }
-  }
-  return res_equal;
-}
-
-int s21_is_greater(s21_decimal decim1, s21_decimal decim2) {
-  int res_greater = 0;
-  int bit1, bit2;
-  if (get_exp(decim1) > get_exp(decim2))
-    res_greater = 1;
-  else
-    for (int bit = ALL_BIT - BITS_IN_INT - 1; bit >= 0; bit--) {
-      if ((bit1 = get_global_bit(decim1, bit)) !=
-          (bit2 = get_global_bit(decim2, bit))) {
-        if (bit1 > bit2)
-          res_greater = 1;
-        break;
-      }
-    }
-  return res_greater;
+  return !s21_is_equal(decim1, decim2);
 }
 
 int s21_is_less(s21_decimal decim1, s21_decimal decim2) {
@@ -47,15 +23,20 @@ int s21_is_less(s21_decimal decim1, s21_decimal decim2) {
     res_less = 1;
   else
     for (int bit = ALL_BIT - BITS_IN_INT - 1; bit >= 0; bit--) {
-      if ((bit1 = get_global_bit(decim1, bit)) !=
+      if ((bit1 = get_global_bit(decim1, bit)) ^
           (bit2 = get_global_bit(decim2, bit))) {
         if (bit1 < bit2)
-          res_less = 1;
+          res_less = bit2;
         break;
       }
     }
   return res_less;
 }
+
+int s21_is_greater(s21_decimal decim1, s21_decimal decim2) {
+  return !s21_is_less_or_equal(decim1, decim2);
+}
+
 int s21_is_greater_or_equal(s21_decimal decim1, s21_decimal decim2) {
   int res = 0;
   if (s21_is_greater(decim1, decim2) == 1)
