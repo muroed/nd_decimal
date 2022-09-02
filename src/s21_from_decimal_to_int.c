1b28aa4a175538b316a5bcdf43dc080d8aa1e0c1
Nazeebo Delma <nazeebod@ox-q5.msk.21-school.ru>
2022-09-02 17:45:58 +0300

    :shirt: refactoring: clang style test

--- src/s21_from_decimal_to_int.c
+++ src/s21_from_decimal_to_int.c
@@ -1,10 +1,11 @@
 #include "s21_decimal.h"
 #include "decimal_core.h"
+
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
     int result = OK;
     int ten_pow = (src.bits[3] << 1) >> 17;
 
-    if(!dst) {
+    if (!dst) {
         result = ERR;
     } else if (ten_pow > 28) {
         *dst = 0;
