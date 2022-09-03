2625e120b4b94bf055e78d65974b34c0edb680b8
Nazeebo Delma <nazeebod@ox-q5.msk.21-school.ru>
2022-09-03 17:55:40 +0300

    :star: add(arifmetic): s21_add function basic add (does not work with a degree)

--- src/s21_arifmetic.c
+++ src/s21_arifmetic.c
@@ -2,6 +2,10 @@
 #include "decimal_core.h"
 #include "s21_decimal.h"
 
+
+// https://russianblogs.com/article/568550142/
+
+
 // int add(int num1, int num2) {
 //     int res = 0, carry = 0;
 //     res = num1^num2;
@@ -16,5 +20,16 @@
 
 int s21_add(s21_decimal decim1, s21_decimal decim2, s21_decimal *result_decimal) {
   unsigned int error_mark = 0;
-  
+  s21_decimal carry;
+  nullify_all_decimal(result_decimal);
+  nullify_all_decimal(&carry);
+
+  *result_decimal = bit_exclusive_or(decim1, decim2);
+  carry = bit_swift_left(bit_and(decim1, decim2), 1);
+  while (is_null_decimal(carry) == 0) {
+    s21_decimal tmp = *result_decimal;
+    *result_decimal = bit_exclusive_or(*result_decimal, carry);
+    carry = bit_swift_left(bit_and(tmp, carry), 1);
+  }
+  return error_mark;
 }
