f519b62500c9c2356937e59c7d0ed19082d02635
Nazeebo Delma <nazeebod@oa-i5.msk.21-school.ru>
2022-09-12 19:26:58 +0300

    :star: add(another): added layout ща агтсешщты

--- src/s21_another.c
+++ src/s21_another.c
@@ -1,9 +1,20 @@
 #include "decimal_core.h"
 #include "s21_decimal.h"
 
-int s21_negative(s21_decimal decim, s21_decimal *result_decimal) {
+int s21_negate(s21_decimal decim, s21_decimal *result_decimal) {
   nullify_all_decimal(result_decimal);
   chang_sign(&decim);
   *result_decimal = decim;
+  return 0;
+}
+
+int s21_truncate(s21_decimal decim, s21_decimal *result_decimal) {
+  return truncate_to_exp(decim, 0, result_decimal);
+}
+int s21_round(s21_decimal decim, s21_decimal *result_decimal) {
+  return 0;
+}
+int s21_floor(s21_decimal decim, s21_decimal *result_decimal) {
+
   return 0;
 }
